680f559387fa7756ba5b07476c6fd7a889a24e34
gigas <gigas5234@gmail.com>
2025-08-21 00:42:46 +0900

    feedback add fixed

--- app/persona/page.jsx
+++ app/persona/page.jsx
@@ -596,7 +596,7 @@ function PersonaPage() {
                             </svg>
                             <span>데이트 상대 선택</span>
                         </a>
-                        <a href="#">
+                        <a href="/feedback">
                             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                                 <path d="M12 20h9"/>
                                 <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"/>
